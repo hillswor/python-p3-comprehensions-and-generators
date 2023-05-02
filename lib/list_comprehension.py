10e5ad26e98a73cae190712a8d281300f9ffa3ce
Bruce <bruce.hillsworth@gmail.com>
2023-05-01 20:58:24 -0600

    Passed Tests

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+import ipdb
+
 
 def return_evens(num_list):
-    pass
+    evens = [n for n in num_list if n % 2 == 0]
+    return evens
+
 
 def make_exclamation(sentence_list):
-    pass
+    excited_strings = [string + "!" for string in sentence_list]
+    return excited_strings
